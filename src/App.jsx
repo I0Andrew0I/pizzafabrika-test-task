e8595d87633b2190892d21f287e199d3ad7aee04
I0Andrew0I <43104794+I0Andrew0I@users.noreply.github.com>
2021-08-26 23:14:19 +0700

    Fix redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router";
+import { Redirect, Route, Switch } from "react-router";
 import EmployeeForm from "./components/EmployeeForm/EmployeeForm";
 import EmployeesList from "./components/EmployeesList/EmployeesList";
 import "./App.scss";
@@ -7,9 +7,10 @@ const App = () => {
   return (
     <main className="app">
       <Switch>
-        <Route path="/employee/add" component={EmployeeForm} />
-        <Route path="/employee/:employeeId" component={EmployeeForm} />
-        <Route path="*" component={EmployeesList} />
+        <Route exact path="/employee/add" component={EmployeeForm} />
+        <Route exact path="/employee/:employeeId" component={EmployeeForm} />
+        <Route exact path="/" component={EmployeesList} />
+        <Redirect to="/" />
       </Switch>
     </main>
   );
